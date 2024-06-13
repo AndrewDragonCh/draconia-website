1bcaaef36ba76a945afe0dfc6717ae3e8c5d83d9
Andrew Still <astill@dragonaere.com>
2024-06-13 18:35:46 -0400

    Added some more reactivity

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import ServerCard from './components/ServerCard';
 function App() {
   return (
     <main className='flex flex-col items-center justify-center w-screen h-screen'>
-      <h1 className='sm:text-5xl sm:mb-8 text-xl mb-4'>Welcome to the World of Draconia</h1>
-      <ServerCard width="w-60 sm:w-80" />
+      <h1 className='sm:text-6xl sm:mb-8 text-xl mb-4'>Welcome to the World of Draconia</h1>
+      <ServerCard width="w-60 sm:w-96" />
     </main>
   );
 }
--- src/components/ServerCard.tsx
+++ src/components/ServerCard.tsx
@@ -20,27 +20,27 @@ function ServerCard({ width }: { width: string }) {
     <div id='Server Status Card' className={`${width}`}>
         <div
           onClick={() => copyToClipboard("play.draconia.world")}
-          className="cursor-pointer relative text-center w-full p-3 border rounded-3xl"
+          className="cursor-pointer relative text-center w-full p-3 border-2 shadow-lg rounded-3xl"
         >
-          <div id='IP' className="text-center mb-2">
+          <div id='IP' className="text-center mb-2 sm:text-xl text-base">
             play.draconia.world
           </div>
-          <div id='Players Online' className="relative w-full bg-gray-200 rounded-full h-7 dark:bg-gray-700 overflow-hidden mt-2 mb-1">
+          <div id='Players Online' className="relative w-full bg-gray-500 rounded-full sm:h-10 h-7 overflow-hidden mt-2 mb-1">
             {serverStatus && serverStatus.players ? (
               <>
-                <span className="text-xs text-white whitespace-nowrap">
+                <span className="relative sm:text-base text-xs text-white whitespace-nowrap sm:-bottom-[.4rem]">
                   {serverStatus.players.online} / {serverStatus.players.max} players online
                 </span>
                 <div
-                  className="bg-blue-600 h-6 rounded-full"
+                  className="bg-gray-900 h-6 rounded-full"
                   style={{ width: `${(serverStatus.players.online / serverStatus.players.max) * 100}%` }}
                 ></div>
               </>
             ) : (
-              <span className="text-xs text-white whitespace-nowrap">Loading...</span>
+              <span className="sm:text-base text-xs text-white whitespace-nowrap">Loading...</span>
             )}
           </div>
-          <div id='Copy Button' className="absolute right-4 p-1 bg-black text-white rounded-md text-xs whitespace-nowrap" >
+          <div id='Copy Button' className="absolute -bottom-3 right-6 z-10 p-1 bg-white text-black rounded-md sm:text-xs text-[.5rem] shadow-inner" >
             {copySuccess || "Click to copy"}
           </div>
         </div>
