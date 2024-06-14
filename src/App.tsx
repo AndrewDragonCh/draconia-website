f4c23d04a788464ea54b7c51ce18b2d08e9131a5
Andrew Still <astill@dragonaere.com>
2024-06-14 16:59:46 -0400

    Moved the BackgroundImage to its own Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import BackgroundImage from './components/BackgroundImage';
 import ServerCard from './components/ServerCard';
 
 function App() {
   return (
     <main className='flex flex-col items-center justify-center w-screen h-screen'>
+      <BackgroundImage />
       <h1 className='sm:text-6xl sm:mb-8 text-xl mb-4'>Welcome to the World of Draconia</h1>
       <ServerCard width="w-60 sm:w-96" />
     </main>
---
+++ src/components/BackgroundImage.tsx
@@ -0,0 +1,24 @@
+function BackgroundImage() {
+  return (
+    <div style={{
+      position: 'fixed',
+      top: 0,
+      left: 0,
+      width: '100%',
+      height: '100%',
+      zIndex: -1,
+    }}>
+      <picture>
+        <source srcSet="/assets/images/background.webp" type="image/webp" />
+        <img src="/assets/images/background.png" alt="Background" style={{ 
+          filter: 'blur(12px) brightness(75%)',
+          width: '100%',
+          height: '100%',
+          objectFit: 'cover',
+        }} />
+      </picture>
+    </div>
+  );
+}
+
+export default BackgroundImage;
