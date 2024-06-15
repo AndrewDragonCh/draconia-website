29957df3d149f9d42fed8229068565e4fb0c68f5
Andrew Still <astill@dragonaere.com>
2024-06-15 14:11:29 -0400

    I hate responsive websites but this looks really good...

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ function App() {
   return (
     <main className='flex flex-col items-center justify-center w-screen h-screen'>
       <BackgroundImage />
-      <h1 className='sm:text-6xl sm:mb-8 text-xl mb-4 drop-shadow-2xl text-white'>Welcome to the World of Draconia</h1>
-      <ServerCard width="w-60 sm:w-96" />
+      <h1 className='xl:text-6xl xl:mb-8 lg:text-4xl lg:mb-7 md:text-3xl md:mb-6 sm:text-2xl sm:mb-5 text-xl mb-4 drop-shadow-2xl text-white'>Welcome to the World of Draconia</h1>
+      <ServerCard width="2xl:w-[32rem] xl:w-[26rem]  lg:w-80 md:w-72 sm:w-64 w-60" />
     </main>
   );
 }
